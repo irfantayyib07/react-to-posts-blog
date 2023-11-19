31983ce7be66995c79ee5afd11534c197281d14a
Irfan <irfantayyib07@gmail.com>
2023-11-19 10:27:38 +0500

    self study done + improvements

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -4,10 +4,10 @@ import { selectAllPosts, selectPostsByUser } from '../posts/postsSlice'
 import { Link, useParams } from 'react-router-dom'
 
 const UserPage = () => {
- const { userId } = useParams()
- const user = useSelector(state => selectUserById(state, Number(userId)))
+ const userId = Number(useParams().userId)
+ const user = useSelector(state => selectUserById(state, userId))
 
- const postsForUser = useSelector(state => selectPostsByUser(state, Number(userId)))
+ const postsForUser = useSelector(state => selectPostsByUser(state, userId))
 
  // const postsForUser = useSelector(state => {
  //  const allPosts = selectAllPosts(state);
