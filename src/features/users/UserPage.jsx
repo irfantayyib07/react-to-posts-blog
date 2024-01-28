1f27d9b99708c97cb47543248c09669c16d9103e
Irfan <irfantayyib07@gmail.com>
2024-01-28 22:25:54 +0500

    NS

--- src/features/users/UserPage.jsx
+++ src/features/users/UserPage.jsx
@@ -5,35 +5,32 @@ import { useGetPostsByUserIdQuery } from '../posts/postsSlice'
 
 const UserPage = () => {
  const { userId } = useParams()
- const user = useSelector(state => selectUserById(state, Number(userId)))
+ const user = useSelector(state => selectUserById(state, +userId))
 
  const {
-  data: postsForUser,
+  data: postsByUserId,
   isLoading,
   isSuccess,
   isError,
   error
  } = useGetPostsByUserIdQuery(userId);
 
- let content;
- if (isLoading) {
-  content = <p>Loading...</p>
- } else if (isSuccess) {
-  const { ids, entities } = postsForUser
-  content = ids.map(id => (
-   <li key={id}>
-    <Link to={`/post/${id}`}>{entities[id].title}</Link>
-   </li>
-  ))
- } else if (isError) {
-  content = <p>{error}</p>;
- }
+ if (isLoading) return <p>Loading...</p>
+ if (isError) return <p>{error}</p>
+
+ const { ids, entities } = postsByUserId
 
  return (
   <section>
    <h2>{user?.name}</h2>
 
-   <ol>{content}</ol>
+   <ol>
+    {isSuccess && ids.map(id => (
+     <li key={id}>
+      <Link to={`/post/${id}`}>{entities[id].title}</Link>
+     </li>
+    ))}
+   </ol>
   </section>
  )
 }
